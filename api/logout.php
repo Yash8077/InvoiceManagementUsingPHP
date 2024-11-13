27aa97d1b660c4a5c07d87851deeac8cf5f71253
Yash8077 <ymishra502@gmail.com>
2024-11-14 04:11:28 +0530

    Fixed logout logic

--- api/logout.php
+++ api/logout.php
@@ -1,13 +1,13 @@
 <?php
 // Start session
 session_start();
-
+$_SESSION['loggedin'] = false;
 // Destroy the session and expire the cookie
 session_unset();
 session_destroy();
 
 // Expire the cookie by setting its expiration time to the past
-setcookie('loggedin', '', time() - 3600, '/'); // Expire cookie immediately
+setcookie('loggedin', 'false', time() - 3600, '/'); // Expire cookie immediately
 
 // Redirect to login page
 header('Location: /');  // Adjust the URL if needed
